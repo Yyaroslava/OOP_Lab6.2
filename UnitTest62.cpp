5e71244db3e75bd5969f88c083eb30ca3e9a7faf
Yaroslava <72552637+Yyaroslava@users.noreply.github.com>
2021-05-03 12:21:20 +0300

    Add files via upload

---
+++ UnitTest62.cpp
@@ -0,0 +1,22 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+#include "../OOP Lab6.2/Decimal.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest62
+{
+	TEST_CLASS(UnitTest62)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			Decimal a1(10, "12312"), a2(10, "32323"), a3(10);
+			a3 = a1 + a2;
+			Assert::AreEqual(a3[0], unsigned char('5'));
+			Assert::AreEqual(a3[3], unsigned char('4'));
+			Assert::AreEqual(a3[2], unsigned char('6'));
+		}
+	};
+}
